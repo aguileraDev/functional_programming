0a30560182578afd6a996a967fa27f8d7491bf9d
Manuel Aguilera <102045443+aguileraDev@users.noreply.github.com>
2025-02-20 17:23:12 -0400

    chore: remove ProductRepository

--- src/main/java/com/practice/fuctional/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practice.fuctional.repositories;
-
-import com.practice.fuctional.models.Product;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends ReactiveCrudRepository<Product, Long> {
-}
